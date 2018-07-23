b5899a199a4d8b1310315c5626e9588b042bebea
GiSDeCain <git@git.com>
2018-07-23 11:20:29 +0200

    find forum login page

--- login_user.py
+++ login_user.py
@@ -4,8 +4,14 @@ import configparser
 config = configparser.ConfigParser()
 config.read("cfg_att.py")
 forum_url = config.get("user", "url")
+user_name = config.get("user", "name")
+passwd = config.get("user", "passwd")
 
-print(forum_url)
+
+def login_user(self):
+    self.wd = WebDriver()
+    wd = self.wd
+    wd.get(forum_url)
 
 
 __author__ = 'GiSDeCain'
