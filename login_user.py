fbead107f97a8b2d289d646eb3692d11233a1eb8
GiSDeCain <git@git.com>
2018-07-24 14:43:25 +0200

    working login with shity structure

--- login_user.py
+++ login_user.py
@@ -1,17 +1,19 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
-import configparser
-
-config = configparser.ConfigParser()
-config.read("cfg_att.py")
-forum_url = config.get("user", "url")
-user_name = config.get("user", "name")
-passwd = config.get("user", "passwd")
+from cfg import config
 
 
-def login_user(self):
-    self.wd = WebDriver()
-    wd = self.wd
-    wd.get(forum_url)
+def setup():
+    wd = WebDriver()
+    wd.get(config.url)
+    wd.find_element_by_id("username").clear()
+    wd.find_element_by_id("username").send_keys(config.userName)
+    wd.find_element_by_id("password").clear()
+    wd.find_element_by_id("password").send_keys(config.passwd)
+    wd.find_element_by_name("login").click()
+    wd.quit()
+
+
+setup()
 
 
 __author__ = 'GiSDeCain'
