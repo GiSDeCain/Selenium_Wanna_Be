832a5a23d26ccd86d5fccb49000d5ee5b2fc382e
GiSDeCain <git@git.com>
2018-07-27 13:32:03 +0200

    tests cleaned from the driver things. conftest and fixture files created. still empty yet.

--- tests/test_check_page_title.py
+++ tests/test_check_page_title.py
@@ -2,11 +2,8 @@ from fixture.methods import *
 
 
 def test_check_page_title():
-    wd = driver()
-    open_main_page(wd)
     get_page_title(wd)
     assert wd.title == config.mainPageTitle
-    close_browser(wd)
 
 
 __author__ = 'GiSDeCain'
--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -2,11 +2,8 @@ from fixture.methods import *
 
 
 def test_login():
-    wd = driver()
-    open_main_page(wd)
     login_user(wd)
     assert get_logged_username(wd) == config.userName
-    close_browser(wd)
 
 
 __author__ = 'GiSDeCain'
