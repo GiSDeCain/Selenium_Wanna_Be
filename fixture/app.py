59c8e3150d3c2ae64e81799eb907e46f0fe57bcc
GiSDeCain <git@git.com>
2018-07-30 08:37:02 +0200

    fixture working. victory. but it is not get base url yest and tests failed.

--- fixture/app.py
+++ fixture/app.py
@@ -6,8 +6,9 @@ class Application:
 
     def __init__(self, base_url="http://forum.attnauka.webd.pro"):
         self.wd = WebDriver()
+        #self.wd.implicitly_wait(10)
         self.base_url = base_url
-        self.methods = Methods()
+        self.methods = Methods(self)
 
     def destroy(self):
         self.wd.close()
