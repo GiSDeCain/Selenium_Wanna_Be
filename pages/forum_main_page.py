5976f5d4aee6cf6fb2dc1911ee1c3eeaa5795ce7
GiSDeCain <git@git.com>
2018-08-01 20:35:20 +0200

    basic task.

--- pages/forum_main_page.py
+++ pages/forum_main_page.py
@@ -9,12 +9,8 @@ class MainPage:
 
     def open_subforum(self):
         wd = self.app.wd
-        try:
-            element = wd.find_element_by_xpath("//a[@class='forumtitle' and text()='%s']" % Config.subforumName)
-            element.click()
-        except NoSuchElementException:
-            print(" Error occured. Navigation element to sub forum not found")
-            return 0
+       wd.find_element_by_xpath("//a[@class='forumtitle' and text()='%s']" % Config.subforumName).click()
+
 
 
 __author__ = 'GiSDeCain'
--- tests/test_enter_subforum.py
+++ tests/test_enter_subforum.py
@@ -3,10 +3,8 @@ from config.cfg import Config
 
 def test_enter_subforum(app):
     app.login_page.login_user(Config.userName, Config.passwd)
-    if app.main_page.open_subforum() != 0:
-        assert app.sub_page.check_if_in_good_subforum() == Config.subforumName
-    else:
-        print("Test not done")
+    app.main_page.open_subforum()
+    assert app.sub_page.check_if_in_good_subforum() == Config.subforumName
 
 
 __author__ = 'GiSDeCain'
