f5dc3439d0fef9d8f8092483e028ee5ffb038b30
GiSDeCain <git@git.com>
2018-08-01 20:43:30 +0200

    basic task corrected again

--- pages/forum_sub_page.py
+++ pages/forum_sub_page.py
@@ -5,7 +5,7 @@ class SubPage:
     def __init__(self, app):
         self.app = app
 
-    def name_of_subforum(self):
+    def get_name_of_subforum(self):
         wd = self.app.wd
         return wd.find_element_by_class_name("forum-title").text
 
--- tests/test_enter_subforum.py
+++ tests/test_enter_subforum.py
@@ -4,7 +4,7 @@ from config.cfg import Config
 def test_enter_subforum(app):
     app.login_page.login_user(Config.userName, Config.passwd)
     app.main_page.open_subforum()
-    assert app.sub_page.name_of_subforum() == Config.subforumName1
+    assert app.sub_page.get_name_of_subforum() == Config.subforumName1
 
 
 __author__ = 'GiSDeCain'
