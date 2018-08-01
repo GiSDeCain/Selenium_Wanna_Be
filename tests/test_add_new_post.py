309dd6ab49785936da5c7b16cc02dbc11c30c9c2
GiSDeCain <gisdecain8@gmail.com>
2018-08-01 22:12:23 +0200

    test add_new_topic just started

--- tests/test_add_new_post.py
+++ tests/test_add_new_post.py
@@ -4,6 +4,6 @@ from config.cfg import Config
 def test_add_post(app):
     app.login_page.login_user(Config.userName, Config.passwd)
     app.main_page.open_subforum()
-
+    #tu będzie reszta testu
 
 __author__ = 'GiSDeCain'
